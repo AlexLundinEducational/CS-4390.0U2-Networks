9157706526529275fc32a9ee0fb8e4990b5f96f6
AlexLundinEducational <31811167+AlexLundinEducational@users.noreply.github.com>
2018-07-09 11:06:20 -0500

    tracking

---
+++ project1/phase1/program5.h
@@ -0,0 +1,40 @@
+//Alex Lundin
+//AML140830@utdallas.edu
+//SE 3377.501 - CC++ in  UNIX
+#ifndef PROGRAM5_H
+#define  PROGRAM5_H
+
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <map>
+#include <iostream>
+#include <csignal>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+extern std::map <int, std::string> myMap;
+using namespace std;
+void parseCommandLine(int, char **);
+void processControl();
+void printMapData();
+enum myEnums {CONFIGFILE,DFLAG, LOGFILE, NUMVERSIONS, VERBOSE, PASSWORD, WATCHDIR};
+#endif // PROGRAM5_H
+
