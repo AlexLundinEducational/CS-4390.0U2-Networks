e1bb992737601de7db81a51a7dce6843f08da139
AlexLundin <aml140830@utdallas.edu>
2018-07-17 00:30:23 -0500

    example complete

--- SE 3377.501 - CC++ in  UNIX/Assignments/Quick Codes and Shell Programs/chmod.sh
+++ SE 3377.501 - CC++ in  UNIX/Assignments/Quick Codes and Shell Programs/chmod.sh
@@ -2,6 +2,4 @@
 #AML140830@utdallas.edu
 #SE 3377.501 - CC++ in  UNIX
 #!/bin/bash
-#replace $file with name to untar
-#tar zxfv $file
 chmod -R 777 ./
--- SE 3377.501 - CC++ in  UNIX/Assignments/Quick Codes and Shell Programs/ctar.sh
+++ SE 3377.501 - CC++ in  UNIX/Assignments/Quick Codes and Shell Programs/ctar.sh
@@ -2,11 +2,12 @@
 #AML140830@utdallas.edu
 #SE 3377.501 - CC++ in  UNIX
 #!/bin/bash
-#this line creates a tarbar from your current directory-name
-#you need to specifcy the name with "my.tar.gz"
+#this line creates a tarbar from your a folder, called folder-name, in your current directory
+#you need to specifcy the tarball name with "my.tar.gz"
 #you need to specify the folder in your current directory with "folder-name"
-#don't put a path on the folder-name
+#don't put a full path on the folder-name
+#make sure the folder called folder-name is visible from the current directory
 tar -zxvf my.tar.gz folder-name
 # untar
-# this line untars the "my.tar.gz" in the current directory
+# this line untars the "my.tar.gz" that is in the current directory
 tar -xvzf my.tar.gz
